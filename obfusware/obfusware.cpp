286329429ed8c26c21c99656bb9a5358043cdf36
clementcharpentier <clement.charpentier@insa-cvl.fr>
2021-02-02 19:05:53 +0100

    cachage de writefile avec beep + jmJarre oxygen part cat Feature

--- obfusware/obfusware.cpp
+++ obfusware/obfusware.cpp
@@ -10,23 +10,11 @@ int _tmain(int argc, _TCHAR* argv[])
     char s[] = "Hello world";
     DWORD dwBytesWritten;
 	WriteFile((HANDLE)0x00000007, s, (DWORD)strlen(s), &dwBytesWritten, NULL);    //prints always to console
-	while(1);
-
-
-
-	//AllocConsole();
-    //HANDLE hStdout;
-    //HANDLE hStdout2 = (HANDLE)0x00000007;
-    //HANDLE hScreenBuffer = CreateFileA("CONOUT$", GENERIC_READ | GENERIC_WRITE, FILE_SHARE_WRITE, NULL, OPEN_EXISTING, 0, NULL);
-
-    //hStdout = GetStdHandle(STD_OUTPUT_HANDLE);
-    //WriteFile(hStdout2, s, (DWORD)strlen(s), &dwBytesWritten, NULL);	//prints always to console
-
-    //WriteFile(hStdout, s, (DWORD)strlen(s), &dwBytesWritten, NULL);     //can be redirected; example TestCON.exe > test.txt
-    //WriteFile(hScreenBuffer, s, (DWORD)strlen(s), &dwBytesWritten, NULL);//prints always to console
-    //FreeConsole();
-	//WriteFile((HANDLE)0x00000007, s, (DWORD)strlen(s), &dwBytesWritten, NULL);
+	printf("\naddr IsDebuggerPresent = %p", IsDebuggerPresent);
+	printf("\naddr WriteFile = %p", WriteFile);
+	int delta = 7C82F6EF - 7C8112FF;
 	
+	while(1);	
 	return 0;
 }
 
